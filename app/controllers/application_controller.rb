001b94db9946b55e6a466857d973fbd4336efe21
shita1112 <a1125ts@aiit.ac.jp>
2019-08-25 17:35:50 +0900

    herokuapp.comへのリクエストは自前ドメインにリダイレクト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :redirect_to_original_domain, if: %i[herokuapp_domain? production?]
+
+  private
+
+  def redirect_to_original_domain
+    redirect_to "https://#{Settings.original_domain}#{request.fullpath}", status: 301
+  end
+
+  def herokuapp_domain?
+    request.domain == Settings.herokuapp_domain
+  end
+
+  def production?
+    Rails.env.production?
+  end
 end
