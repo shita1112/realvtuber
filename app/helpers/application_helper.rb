2d30c80034ef4a3d9dfddb63c457ba633d63ac08
shita1112 <a1125ts@aiit.ac.jp>
2019-08-27 03:09:37 +0900

    meta-tagsのセットアップ

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def display_meta_tags_default
+    display_meta_tags(
+      site: "リアルバーチャルYoutuber",
+      separator: "|",
+      reverse: true,
+    )
+  end
 end
