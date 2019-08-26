36c96c6e46056446adc5ae39f83195a6db250761
shita1112 <a1125ts@aiit.ac.jp>
2019-08-27 01:16:48 +0900

    自動生成されるファイルの整理

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,24 @@ module Realvtuber
     config.time_zone = "Tokyo"
 
     config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.skip_routes true
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: false,
+                       system_specs: true,
+                       model_specs: true,
+                       fixtures: true
+      g.fixture_replacement :factory_bot,
+                            dir: "spec/factories"
+    end
+
   end
 end
