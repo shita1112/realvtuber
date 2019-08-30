d86cdab6f83363723da269172a704256d838ad82
shita1112 <a1125ts@aiit.ac.jp>
2019-08-30 15:54:58 +0900

    MDBのセットアップ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,8 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
+// default
 require("@rails/ujs").start()
+// require("./hello_vue.js")
 
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+// addition
+// require("popper")
+require("bootstrap")
+require("mdb")
