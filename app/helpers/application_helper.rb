5b21e6e31d0434cc8c69315cbc305aee9c94aa08
shita1112 <a1125ts@aiit.ac.jp>
2019-09-10 13:37:30 +0900

    ログイン画面ではnavバー出さない

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,4 +28,17 @@ module ApplicationHelper
   def ago_if_datetime(val)
     val.is_a?(Time) ? ago(val) : val
   end
+
+  def render_navbar
+    login_page            = current_page?(controller: "devise/sessions",      action: "new")
+    signup_page           = current_page?(controller: "devise/registrations", action: "new")
+    forgot_password_page  = current_page?(controller: "devise/passwords",     action: "new")
+    change_password_page  = current_page?(controller: "devise/passwords",     action: "edit")
+
+    if login_page || signup_page || forgot_password_page || change_password_page
+      render "unobtrusive_navbar"
+    else
+      render "navbar"
+    end
+  end
 end
